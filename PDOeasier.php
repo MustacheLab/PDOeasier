58eb4b429969825f79db912cc84cd020d066efc1
Mustache Lab <kontakt@mustachelab.pl>
2015-08-20 14:35:34 +0200

    new version
    add function dsn() to generate Data Source Name (DSN) from array

---
+++ PDOeasier.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * PDO easier 
+ * few features to work with PHP Data Objects (PDO).
+ *
+ * @author Mustache Lab <http://mustachelab.pl>
+ */
+
+class PDOeasier 
+{
+    protected $dsn, $username, $password, $pdo, $driver_options;
+    
+    public function __construct($dsn, $username = "", $password = "", $driver_options = array())
+    {
+        $this->dsn = $dsn;
+        $this->username = $username;
+        $this->password = $password;
+        $this->driver_options = $driver_options;
+    }
+
+    public function dsn($type, array $params)
+    {
+        return "$type:" . join(';', array_map(function($v, $k) {
+            return "$k=$v";
+        }, $params, array_keys($params)));
+    }
+
+    public function __call($name, array $arguments)
+    {
+        try {
+            $this->connection()->query("SHOW STATUS;")->execute();
+        } catch(\PDOException $e) {
+            if($e->getCode() != 'HY000' || !stristr($e->getMessage(), 'server has gone away')) {
+                throw $e;
+            }
+            $this->reconnect();
+        }
+        return call_user_func_array(array($this->connection(), $name), $arguments);
+    }
+
+    protected function connection()
+    {
+        return $this->pdo instanceof \PDO ? $this->pdo : $this->connect();
+    }
+
+    public function connect()
+    {
+        $this->pdo = new PDO($this->dsn, $this->username, $this->password, (array) $this->driver_options); 
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $this->pdo;
+    }
+
+    public function reconnect()
+    {
+        $this->pdo = null;
+        return $this->connect();
+    }
+}
